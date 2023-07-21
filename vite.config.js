7c04f936fb0df21b2619384a6ec1a7c2b545ca10
Meriem1551 <meriem200409@gmail.com>
2023-07-21 17:58:28 +0200

    deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Simple-onlineShopping-page-using-Chakra-Ui/',
   plugins: [react()],
 })
